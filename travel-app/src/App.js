34fe28e15cecd1dfb223cb39faeb304e34fa52b3
vm799 <vaishalimehmi@yahoo.co.uk>
2022-03-07 17:36:37 +0000

    designing header

--- travel-app/src/App.js
+++ travel-app/src/App.js
@@ -1,10 +1,13 @@
 import Header from "./components/Header/Header"
+import Sidebar from "./components/Sidebar/Sidebar"
 
 function App() {
   return (
-    <div className="App">
-      boooyakahsssa
+    <div className="app">
       <Header />
+      <div className= "app__page">
+        <Sidebar />
+        </div>
      
 
     </div>
--- travel-app/src/components/Header/Header.js
+++ travel-app/src/components/Header/Header.js
@@ -8,17 +8,25 @@ import Avatar from '@material-ui/core/Avatar'
 
 function Header() {
   return (
-    <div className="header">
-       
+    <div className="header container_df_ai_jc">
+       <div className="header_left">
     <MenuIcon />
     <img className= 'header__logo' src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Logo_of_YouTube_%282011-2013%29.svg/800px-Logo_of_YouTube_%282011-2013%29.svg.png?20170830032119" alt="youtube logo"/>
-    <input type="text" />
-    <SearchIcon />
-    <VideoCallIcon />
-    <AppsIcon />
-    <NotificationsIcon />
-    <Avatar alt="V" src='' />
+    </div>
 
+    <div className="header__input container_df_ai">
+    <input placeholder="Search" type="text" />
+    <SearchIcon  className="header__inputButton"/>
+    </div>
+   
+    <div className="header__icons container_df_ai">
+    <VideoCallIcon className="header__icon" />
+    <AppsIcon className="header__icon"/>
+    <NotificationsIcon className="header__icon"/>
+    <Avatar className="header__avatar" alt="V" src='' />
+ 
+
+    </div>
     </div>
   )
 }
